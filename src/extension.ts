bf9086914aa5b54966922437d4bab4a748e5d76d
takuyaw-w <takuya.jamtb@gmail.com>
2019-09-20 01:01:14 +0900

    wip

--- src/extension.ts
+++ src/extension.ts
@@ -5,10 +5,23 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "swapman" is now active!');
 
 	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+		
 
-		vscode.window.showInformationMessage('Hello World!');
-		const test = vscode.window.activeTextEditor;
-		console.log(test);
+		const editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
+		if (!editor) {
+			return;
+		}
+
+		const doc: vscode.TextDocument = editor.document;
+		const b: vscode.Selection[] = editor.selections;
+		console.log(b[0]);
+		const start: vscode.Position = new vscode.Position(b[0].start.line, b[0].start.character);
+		const end: vscode.Position = new vscode.Position(b[0].end.line, b[0].end.character);
+		const range: vscode.Range = new vscode.Range(start, end);
+		console.log(range);
+		let a = doc.getText(range);
+		console.log(a);
+		vscode.window.showInformationMessage("aaa");
 	});
 
 	context.subscriptions.push(disposable);
