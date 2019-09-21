636b63829cd9568fadfb7a5c285ff78be775ddee
takuyaw-w <takuya.jamtb@gmail.com>
2019-09-21 20:34:14 +0900

    wip

--- src/extension.ts
+++ src/extension.ts
@@ -10,15 +10,15 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 
 		const doc: vscode.TextDocument = editor.document;
-		const selectedNumber: vscode.Selection[] = editor.selections;
-		if (selectedNumber.length > 2) {
-			vscode.window.showErrorMessage("Exceeded selection.");
+		const selections: vscode.Selection[] = editor.selections;
+		if (selections.length > 1 && selections.length < 3) {
+			vscode.window.showErrorMessage("Invalid number of selections.");
 			return;
 		}
-		const firstStartPosition: vscode.Position = new vscode.Position(selectedNumber[0].start.line, selectedNumber[0].start.character);
-		const firstEndPosition: vscode.Position = new vscode.Position(selectedNumber[0].end.line, selectedNumber[0].end.character);
-		const secondStartPosition: vscode.Position = new vscode.Position(selectedNumber[1].start.line, selectedNumber[1].start.character);
-		const secondEndPosition: vscode.Position = new vscode.Position(selectedNumber[1].end.line, selectedNumber[1].end.character);
+		const firstStartPosition: vscode.Position = new vscode.Position(selections[0].start.line, selections[0].start.character);
+		const firstEndPosition: vscode.Position = new vscode.Position(selections[0].end.line, selections[0].end.character);
+		const secondStartPosition: vscode.Position = new vscode.Position(selections[1].start.line, selections[1].start.character);
+		const secondEndPosition: vscode.Position = new vscode.Position(selections[1].end.line, selections[1].end.character);
 		const firstElementRange: vscode.Range = new vscode.Range(firstStartPosition, firstEndPosition);
 		const secondElementRange: vscode.Range = new vscode.Range(secondStartPosition, secondEndPosition);
 		let firstElement: string = doc.getText(firstElementRange);
