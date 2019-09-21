ee303d6401395c2ea905be53beb0c89602b59d27
takuyaw-w <takuya.jamtb@gmail.com>
2019-09-21 21:21:53 +0900

    WIP

--- src/extension.ts
+++ src/extension.ts
@@ -11,24 +11,25 @@ export function activate(context: vscode.ExtensionContext) {
 
 		const doc: vscode.TextDocument = editor.document;
 		const selections: vscode.Selection[] = editor.selections;
-		if (selections.length === 1 && selections.length > 3) {
+		if (selections.length > 1 && selections.length < 3) {
+			const firstStartPosition: vscode.Position = new vscode.Position(selections[0].start.line, selections[0].start.character);
+			const firstEndPosition: vscode.Position = new vscode.Position(selections[0].end.line, selections[0].end.character);
+			const secondStartPosition: vscode.Position = new vscode.Position(selections[1].start.line, selections[1].start.character);
+			const secondEndPosition: vscode.Position = new vscode.Position(selections[1].end.line, selections[1].end.character);
+			const firstElementRange: vscode.Range = new vscode.Range(firstStartPosition, firstEndPosition);
+			const secondElementRange: vscode.Range = new vscode.Range(secondStartPosition, secondEndPosition);
+			let firstElement: string = doc.getText(firstElementRange);
+			let secondElement: string = doc.getText(secondElementRange);
+			[firstElement, secondElement] = [secondElement, firstElement];
+			editor.edit(builder => {
+				builder.replace(firstElementRange, firstElement);
+				builder.replace(secondElementRange, secondElement);
+			});
+			vscode.window.showInformationMessage("Swaaaaaaaaaaaaaaaaaap!!!!!");
+		} else {
 			vscode.window.showErrorMessage("Invalid number of selections.");
 			return;
 		}
-		const firstStartPosition: vscode.Position = new vscode.Position(selections[0].start.line, selections[0].start.character);
-		const firstEndPosition: vscode.Position = new vscode.Position(selections[0].end.line, selections[0].end.character);
-		const secondStartPosition: vscode.Position = new vscode.Position(selections[1].start.line, selections[1].start.character);
-		const secondEndPosition: vscode.Position = new vscode.Position(selections[1].end.line, selections[1].end.character);
-		const firstElementRange: vscode.Range = new vscode.Range(firstStartPosition, firstEndPosition);
-		const secondElementRange: vscode.Range = new vscode.Range(secondStartPosition, secondEndPosition);
-		let firstElement: string = doc.getText(firstElementRange);
-		let secondElement: string = doc.getText(secondElementRange);
-		[firstElement, secondElement] = [secondElement, firstElement];
-		editor.edit(builder => {
-			builder.replace(firstElementRange, firstElement);
-			builder.replace(secondElementRange, secondElement);
-		});
-		vscode.window.showInformationMessage("Swaaaaaaaaaaaaaaaaaap!!!!!");
 	});
 
 	context.subscriptions.push(disposable);
